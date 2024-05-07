d12777ad99714fcb273d586f9422c2a0543ba93d
Dhruv <dhruvvk2326@gmail.com>
2024-05-07 14:46:25 +0530

    UI-DEVELOPMENT using tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,33 @@ function App() {
 
     return (
         <>
+            {/* NAVBAR */}
             <Navbar />
+
+            {/* MAIN */}
+            <div className='container mx-auto my-5 rounded-xl p-5 bg-violet-200'>
+                <div className='addToDo'>
+                    <h2 className='text-lg font-bold'>Add a todo</h2>
+                    <input className='w-80 ' type='text' />
+                    <button className='bg-violet-800 hover:bg-violet-950 p-3 py-1 text-white rounded-md mx-4'>
+                        ADD
+                    </button>
+                </div>
+                <h2 className='text-lg font-bold'>YOUR TODOS</h2>
+                <div className='todos'>
+                    <div className='todo flex'>
+                        <div className='text'></div>
+                        <div className='buttons'>
+                            <button className='bg-violet-800 hover:bg-violet-950 p-3 py-1 text-white rounded-md mx-4'>
+                                EDIT
+                            </button>
+                            <button className='bg-violet-800 hover:bg-violet-950 p-3 py-1 text-white rounded-md mx-4'>
+                                DELETE
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
